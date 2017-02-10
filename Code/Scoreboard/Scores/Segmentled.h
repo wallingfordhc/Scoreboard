f73691e2740dcfb891c19f4ce6a21f2e9e3816de
David Shannon <dzshanno@yahoo.com>
2017-02-10 08:36:18 +0000

    Create Segmentled.h

---
+++ Code/Scoreboard/Scores/Segmentled.h
@@ -0,0 +1,4 @@
+// FUNCTION TO CONTROL CHAIN OF LEDs AS A 7 SEGMENT DISPLAY
+// FASTLED.h TO CONTROL THE LED CHAIN
+//
+// designed to translate reuests to display a simple number into calls to fastled functions to display the right leds in
