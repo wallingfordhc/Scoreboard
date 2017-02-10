05e40c5f1d657d26aa5b95f4b0ac4b3908b413eb
David Shannon <dzshanno@yahoo.com>
2017-02-10 08:39:20 +0000

    Update Segmentled.h

--- Code/Scoreboard/Scores/Segmentled.h
+++ Code/Scoreboard/Scores/Segmentled.h
@@ -1,4 +1,8 @@
 // FUNCTION TO CONTROL CHAIN OF LEDs AS A 7 SEGMENT DISPLAY
 // FASTLED.h TO CONTROL THE LED CHAIN
 //
-// designed to translate reuests to display a simple number into calls to fastled functions to display the right leds in
+// designed to translate reuests to display a simple number into calls to fastled functions to display the right leds in a strip of LEDs
+//
+// may also allow for animated transitions from one score to annother
+//
+// REQUIRES LED STRIP TO BE SET OUT IN A STANDARD CONFIGURATION
